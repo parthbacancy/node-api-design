84848197b84bf64f97ea50961b177a36e7d59e95
Parth Patel <parth.patel1@bacancy.com>
2024-07-04 10:52:09 +0530

    port config changes

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ import app from "./server";
 console.log(config);
 
 app.listen(config.port, () => {
-  console.log("hello on http://localhost:3002");
+  console.log(`hello on http://localhost:${config.port}`);
 });
